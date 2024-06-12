233e91e99c201cd302d55f98a675e1f189a43441
akramanayatov@gmail.com <akramanayatov@gmail.com>
2024-06-12 14:52:20 +0300

    add db model  add database in App

---
+++ AnayatovHouseholdService/Classes/ConnectionDataBase.cs
@@ -0,0 +1,14 @@
+﻿using AnayatovHouseholdService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnayatovHouseholdService.Classes
+{
+    internal class ConnectionDataBase
+    {
+        public static HouseholdServiceEntities connection = new HouseholdServiceEntities();
+    }
+}
